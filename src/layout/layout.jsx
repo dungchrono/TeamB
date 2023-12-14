73f2a12479b5f2ad885b46fa3b6ffed9519b8ace
Hoang Anh Dung <dungchrono@gmail.com>
2023-12-14 09:17:45 +0700

    1

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Header.';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { useMyContext } from '../hook/MyContext';
 import { useNavigate } from 'react-router';
